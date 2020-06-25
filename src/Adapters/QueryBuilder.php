49b7db89489bc81368b87a1907a028d99a7f07c3
sitchi <nikoloz.sitchinava@gmail.com>
2020-06-25 11:37:14 +0400

    Update QueryBuilder.php

--- src/Adapters/QueryBuilder.php
+++ src/Adapters/QueryBuilder.php
@@ -1,49 +1,54 @@
 <?php
+
 namespace DataTables\Adapters;
+
 use Phalcon\Paginator\Adapter\QueryBuilder as PQueryBuilder;
 
-class QueryBuilder extends AdapterInterface{
-  protected $builder;
+class QueryBuilder extends AdapterInterface
+{
+    protected $builder;
 
-  public function setBuilder($builder) {
-    $this->builder = $builder;
-  }
+    public function setBuilder($builder)
+    {
+        $this->builder = $builder;
+    }
 
-  public function getResponse() {
-    $builder = new PQueryBuilder([
-      'builder' => $this->builder,
-      'limit'   => 1,
-      'page'    => 1,
-    ]);
+    public function getResponse()
+    {
+        $builder = new PQueryBuilder([
+            'builder' => $this->builder,
+            'limit' => 1,
+            'page' => 1,
+        ]);
 
-    $total = $builder->getPaginate();
+        $total = $builder->paginate();
 
-    $this->bind('global_search', function($column, $search) {
-      $this->builder->orWhere("{$column} LIKE :key_{$column}:", ["key_{$column}" => "%{$search}%"]);
-    });
+        $this->bind('global_search', function ($column, $search) {
+            $this->builder->orWhere("{$column} LIKE :key_{$column}:", ["key_{$column}" => "%{$search}%"]);
+        });
 
-    $this->bind('column_search', function($column, $search) {
-      $this->builder->andWhere("{$column} LIKE :key_{$column}:", ["key_{$column}" => "%{$search}%"]);
-    });
+        $this->bind('column_search', function ($column, $search) {
+            $this->builder->andWhere("{$column} LIKE :key_{$column}:", ["key_{$column}" => "%{$search}%"]);
+        });
 
-    $this->bind('order', function($order) {
-      if (!empty($order)) {
-        $this->builder->orderBy(implode(', ', $order));
-      }
-    });
+        $this->bind('order', function ($order) {
+            if (!empty($order)) {
+                $this->builder->orderBy(implode(', ', $order));
+            }
+        });
 
-    $builder = new PQueryBuilder([
-      'builder' => $this->builder,
-      'limit'   => $this->parser->getLimit(),
-      'page'    => $this->parser->getPage(),
-    ]);
+        $builder = new PQueryBuilder([
+            'builder' => $this->builder,
+            'limit' => $this->parser->getLimit(),
+            'page' => $this->parser->getPage(),
+        ]);
 
-    $filtered = $builder->getPaginate();
+        $filtered = $builder->paginate();
 
-    return $this->formResponse([
-      'total'     => $total->total_items,
-      'filtered'  => $filtered->total_items,
-      'data'      => $filtered->items->toArray(),
-    ]);
-  }
+        return $this->formResponse([
+            'total' => $total->total_items,
+            'filtered' => $filtered->total_items,
+            'data' => $filtered->items->toArray(),
+        ]);
+    }
 }
